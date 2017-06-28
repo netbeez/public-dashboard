e45ab4ed3a83a8c0c9493a38c9adfe45a3ea5f51
Allison Jones <allison@netbeez.net>
2017-06-28 11:10:59 -0400

    set up config

--- public-dashboard/config.php
+++ public-dashboard/config.php
@@ -15,19 +15,19 @@ define("COMPANY_NAME", "Acme Company");
 define("SITE_DESCRIPTION", "This description is declared in the config file!");
 
 //The URL of this site
-define("SITE_URL", "/");
+define("SITE_URL", "http://172.30.0.49/public-dashboard-github/public-dashboard/");
 
 //The host address of the NetBeez API (this is usually your NB dashboard's hostname)
-define("API_HOST", "<YOUR_NETBEEZ_HOSTNAME>");
+define("API_HOST", "https://172.30.0.7:3001");
 
 //The NetBeez API version
 define("API_VERSION", "v1");
 
 //Your authentication key for accessing the API
-define("API_AUTH_KEY", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+define("API_AUTH_KEY", "dc37ae6ff9d7fb875f131d0832a990310a8ec769");
 
 //Boolean setting for cURL option to verify the SSL host
-define("SSL_VERIFY_HOST", true);
+define("SSL_VERIFY_HOST", false);
 
 //Boolean setting for cURL option to verify the SSL peer.
 //Default is false due to issue with certificate configuration on internal NetBeez instances
